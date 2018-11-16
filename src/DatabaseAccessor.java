6fb3135800ba2f991c6ecbdb3055bd62db6402bf
Kailash Baas <kailash.baas@gmail.com>
2018-11-16 10:44:20 -0800

    Begin work on database accessor

---
+++ src/DatabaseAccessor.java
@@ -0,0 +1,93 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class DatabaseAccessor
+{
+    private static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private static final String DB_URL = "jdbc:oracle:thin@cloud-34-133.eci.ucsb.edu:1521:XE";
+    private static final String USERNAME = "@cloud-34-133.eci.ucsb.edu";
+    private static final String PASSWORD = "";
+
+    public static ResultSet query_db(String query)
+    {
+        Connection conn = null;
+        Statement stmt = null;
+
+        try
+        {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            stmt = conn.createStatement();
+
+            return stmt.executeQuery(query);
+        }
+        catch (SQLException se)
+        {
+            se.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (stmt != null)
+                {
+                    conn.close();
+                }
+            }
+            catch (SQLException se)
+            {
+            }
+            try
+            {
+                if (conn != null)
+                {
+                    conn.close();
+                }
+            }
+            catch (SQLException se)
+            {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    public static void insert_acct(Account acct, ArrayList<Customer> owners)
+    {
+
+    }
+
+    public static void insert_transaction(Transaction t, Customer initiatior)
+    {
+
+    }
+
+    public static void insert_check(Check c)
+    {
+
+    }
+
+    public static boolean update_acct(Account acct)
+    {
+
+    }
+
+    public static boolean update_customer(Customer c)
+    {
+
+    }
+
+    public static boolean delete_acct(Account acct)
+    {
+
+    }
+
+    public static boolean delete_customer(Customer c)
+    {
+
+    }
+
+}
